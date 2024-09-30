377faa9ae414af362ba5d1bcbd3c2ec04de86f10
AkmalIT <akmalrustamov0203@gmail.com>
2024-09-30 22:54:34 +0500

    first commit

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { collectDefaultMetrics, register } from 'prom-client';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  collectDefaultMetrics();
+  app
+    .getHttpAdapter()
+    .getInstance()
+    .get('/metrics', async (req, res) => {
+      res.set('Content-Type', register.contentType);
+      res.end(await register.metrics());
+    });
+
+  await app.listen(3001);
+}
+bootstrap();
